fddeeeb47a28f9c8642927176d052669b1462d99
boudradar <hamza.boudradar@etu.unice.fr>
2019-01-04 17:08:30 +0100

    Fragments / connexion

---
+++ app/src/main/java/com/example/mbdse/firstapp/MainActivityFragments.java
@@ -0,0 +1,94 @@
+package com.example.mbdse.firstapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements ICallable{
+
+    FrameLayout fl;
+    FrameLayout fl1;
+    EditText login;
+    EditText mdp;
+
+    boolean a = true;
+
+    MainActivityFragment maf = new MainActivityFragment();
+    SecActivityFragment sec = new SecActivityFragment();
+    private TextAdapter mDbHelper;
+
+    private Database db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mDbHelper = new TextAdapter(new ArrayList<String>());
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fl = (FrameLayout) findViewById(R.id.fragmentHolder);
+        fl1 = (FrameLayout) findViewById(R.id.fragmentHolder1);
+        db = Database.getInstance(getApplicationContext());
+        login = (EditText) findViewById(R.id.login_box);
+        mdp = (EditText) findViewById(R.id.pass_box);
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)//double fragments
+        {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(fl.getId(),maf);
+            fragmentTransaction.replace(fl1.getId(),sec);
+            fragmentTransaction.commit();}
+            else {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(fl.getId(),maf);
+            fragmentTransaction.commit();
+        }
+            return;
+        }
+
+
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"fragment actif",Toast.LENGTH_SHORT).show();
+        mDbHelper.notifyDataSetChanged();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.show(maf);
+
+}
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                addData();
+////                a= !a;
+////                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+////                if (a)
+////                {
+////                    fragmentTransaction.replace(fl.getId(),maf);
+////                } else {
+////                    fragmentTransaction.replace(fl.getId(),sec);
+////                }
+////                fragmentTransaction.commit();
+//            }
+//        });
+
+
+
+    @Override
+    public void transferData(String s) {
+        sec.setText(s);
+    }
+
+
+}
+
