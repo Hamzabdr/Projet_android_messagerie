20c4b8cffc2646caf084fab56abaed01326133ac
boudradar <hamza.boudradar@etu.unice.fr>
2019-01-06 21:54:37 +0100

    Receiver

---
+++ app/src/main/java/com/example/mbdse/firstapp/service/Receiver.java
@@ -0,0 +1,14 @@
+package com.example.mbdse.firstapp.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class Receiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i("SERVICE", "Stops Ohhhhhhh");
+            context.startService(new Intent(context, MessageService.class));;
+        }
+    }
