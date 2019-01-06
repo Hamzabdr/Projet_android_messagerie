9568cb33688602fb2b0d1d39ba68777f29fb8734
boudradar <hamza.boudradar@etu.unice.fr>
2019-01-06 19:40:18 +0100

    clear msgtxt

--- app/src/main/java/com/example/mbdse/firstapp/MessagesFragment.java
+++ app/src/main/java/com/example/mbdse/firstapp/MessagesFragment.java
@@ -3,6 +3,7 @@ package com.example.mbdse.firstapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -75,7 +76,7 @@ public class MessagesFragment extends Fragment {
 
         Message msg = new Message(body,"Hamza",true);
             msgtext.getText().clear();
-
+            msgtext.setText("");
             msgAdapter.add(msg);
             messages.setSelection(messages.getCount() - 1);
 
